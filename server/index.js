de43638a7264d6898ead931cc6e9f7e9830cef83
fengzi2016 <1820166258@qq.com>
2018-08-28 16:37:51 +0800

    feat:add server

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const send = require("koa-send");
+const Koa = require("koa");
+const Router = require("koa-router");
+const userAgent = require("koa-useragent");
+const path = require("path");
+const swig = require("swig");
+const compression = require("koa-compress");
+const router = new Router();
+const app = new Koa();
+
+const templateRoot = path.join(__dirname, "../dist/templates");
+
+app.use(userAgent);
+app.use(compression());
+router.get(/^\/(.*)$/, ctx => { 
+    const template = swig.compileFile(path.resolve(templateRoot, "index.html"));
+    ctx.body = template({});
+  
+});
+
+router.get(/^\/static(?:\/|$)/, async ctx => {
+  let filepath = ctx.path.replace(/static\//, "");
+  await send(ctx, filepath, {
+    root: path.join(__dirname, "../dist")
+  });
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(3000);
+console.log("listening on port 3000");
